fffcb89d67e464fdc8f8a7cd5b90d7f01fcdba37
DzakyNaufal <dzakynmulna@gmail.com>
2025-01-26 14:05:29 +0700

    commit 2.1

--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,34 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\BukuController;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AgamaController;
-use App\Http\Controllers\CetakController;
 use App\Http\Controllers\PenulisController;
+use App\Http\Controllers\BukuController;
+use App\Http\Controllers\CetakController;
 use App\Http\Controllers\RegistrasiController;
+use App\Http\Controllers\AuthController; // Import AuthController
+use App\Http\Controllers\AdminController; // Import AdminController
+use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::resource('penulis', PenulisController::class);
-Route::resource('buku', BukuController::class);
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
-Route::resource('registrasi', RegistrasiController::class);
-Route::resource('agama', AgamaController::class);
-
-
+// User login route
 Route::post('/user/login', [AuthController::class, 'userLogin'])->name('user.login');
 
 // Admin login route
 Route::get('/admin/login', [AuthController::class, 'adminLoginPage'])->name('admin.login.page');
 Route::post('/admin/login', [AuthController::class, 'adminLogin'])->name('admin.login');
 
-// Other existing routes...
+// Admin dashboard route
+Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+
+// Logout route
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+
+
 Route::get('/cetak/{id}', [CetakController::class, 'cetak'])->name('cetak');
+Route::resource('penulis', PenulisController::class);
+Route::resource('buku', BukuController::class);
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
+Route::resource('registrasi', RegistrasiController::class);
+Route::resource('agama', AgamaController::class);
 
-
+?>
